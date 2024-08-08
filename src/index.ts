328ed569028a44a0a3c7fcfcb4caf4b07ac31651
john nabil <mahatozy@gmail.com>
2024-08-08 13:10:26 +0200

    initial commit +user feature

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose, { connect } from "mongoose";
+import userRoute from "./routes/userRoute";
+
+const app = express();
+const port = 3001;
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/ecommerce").then(() => {
+  console.log("data base connected");
+});
+
+app.use('/user',userRoute)
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
